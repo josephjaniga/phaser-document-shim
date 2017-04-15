73f591e714ff6182deda0913a11136f5298ac1d2
Joseph Janiga <Joseph Janiga>
2017-04-15 14:43:56 -0400

    WIP

--- index.js
+++ index.js
@@ -6,7 +6,29 @@
 
 var documentObject = {
     'createElement': function () {
-        return {width:0, height:0};
+
+        return {
+            width:0,
+            height:0,
+            getContext: function () {
+
+                return {
+                    setTransform: function(){},
+                    fillRect: function(){},
+                    getImageData: function(){
+
+                        return {
+                            data: {}
+                        };
+
+                    },
+                    putImageData: function(){},
+                    drawImage: function(){},
+                };
+
+            }
+        };
+
     },
 };
 
